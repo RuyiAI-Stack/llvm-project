d6338b115126b9b5c4ae7c627ce5d4a33f931b89
WuXintong123 <13683168028@163.com>
2026-07-31 01:48:06 +0800

    [SyncBots] Integrate LLVM at 11b58f0

--- openmp/runtime/test/transform/fuse/iterfor.cpp
+++ openmp/runtime/test/transform/fuse/iterfor.cpp
@@ -183,14 +183,6 @@ int main() {
 // CHECK-NEXT: [D] iterator deref: 2
 // CHECK-NEXT: vv=2
 // CHECK-NEXT: [D] iterator dtor
-// CHECK-NEXT: [C] iterator distance: 3
-// CHECK-NEXT: [C] iterator advance: 0 += 3
-// CHECK-NEXT: [C] iterator move assign
-// CHECK-NEXT: [D] iterator distance: 3
-// CHECK-NEXT: [D] iterator advance: 0 += 3
-// CHECK-NEXT: [D] iterator move assign
-// CHECK-NEXT: [D] iterator dtor
-// CHECK-NEXT: [C] iterator dtor
 // CHECK-NEXT: [D] iterator dtor
 // CHECK-NEXT: [D] iterator dtor
 // CHECK-NEXT: [C] iterator dtor
